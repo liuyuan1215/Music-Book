9b66be17fd149880d297aab90898fbbd56954f92
刘园215 <1803656569@qq.com>
2019-04-10 14:09:04 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,36 +2,46 @@ const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-    mode:'development',
-    entry:{
-        index:'./src/index.js'
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
     },
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'[name].js'
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
+                test: /\.css$/,
                 // use:['style-loader','css-loader']
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
                     use: "css-loader"
-                  })
+                })
+            },
+            {
+                test: /\.(jpg|png|gif)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 5000,
+                        outputPath: '/images'
+                    }
+                }]
             }
         ]
     },
-    plugins:[
+    plugins: [
         new HtmlPlugin({
-            template:'./src/index.html',
-            hash:true
+            template: './src/index.html',
+            hash: true
         }),
         new ExtractTextPlugin("index.css"),
     ],
-    devServer:{
-        contentBase:path.resolve(__dirname,'dist'),
-        host:'localhost',
-        port:8081
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: 'localhost',
+        port: 8081
     }
 }
