797be935dce281366da4f93b6fbdcc990059de5a
刘园215 <1803656569@qq.com>
2019-04-24 21:04:54 +0800

    vue

--- vuecli3/src/store.js
+++ vuecli3/src/store.js
@@ -5,10 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    count:0,
+    msg:'112233'
   },
   mutations: {
-
+    add(state,num){
+      state.count+=num;
+    },
+    reduce(state,num){
+      state.count-=num;
+    }
+  },
+  getters:{
+    num(state){
+      return state.count+state.msg;
+    },
+    xx(state){
+      return state.count*20;
+    }
   },
   actions: {
 
---
+++ vuecli3/src/views/Count.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <!-- {{$store.state.count}} -->
+
+        {{count}}
+        <br>
+        {{msg}}
+        <br>
+        {{num}}
+        <br>
+        {{xx}}
+        <br>
+        <button @click='add(5)'>+++</button>
+        <!-- <button @click='$store.commit("reduce",2)'>---</button> -->
+        <button @click='reduce(2)'>---</button>
+    </div>
+</template>
+
+<script>
+    import {mapState,mapMutations,mapGetters} from 'vuex';
+    export default {
+        computed:{
+            // count(){
+            //     return this.$store.state.count;
+            // }
+            ...mapState(['count','msg']),
+            ...mapGetters(['num','xx'])
+        },
+        methods:{
+            show(){
+                console.log(this.$store.state.count);
+            },
+            fn(num){
+                this.$store.commit('add',num);
+            },
+            ...mapMutations(['add','reduce'])
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
