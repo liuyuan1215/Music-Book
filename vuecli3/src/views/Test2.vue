488946e2a11e845d70a322b1de4317469cccd345
刘园215 <1803656569@qq.com>
2019-04-24 15:08:33 +0800

    vue

---
+++ vuecli3/src/views/Test2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>2222</P>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
