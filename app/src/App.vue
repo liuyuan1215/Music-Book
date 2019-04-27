1c1a7594fe41b77310f309a0d411335e3a46fb63
刘园215 <1803656569@qq.com>
2019-04-27 22:02:12 +0800

    vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,69 @@
 <template>
   <div id="app">
-    <common-header></common-header>
-    <common-footer></common-footer>
+    <common-header :selectMenu='selectMenu'></common-header>
+    <div id="content">
+      <router-view></router-view>
+    </div>
+    <common-footer :menu='menu' :selectMenu='selectMenu' @change='fn'></common-footer>
   </div>
 </template>
 
 <script>
-  import CommonHeader from '@/components/CommonHeader.vue';
-  import CommonFooter from '@/components/CommonFooter.vue';
-  export default {
-    components: {
-      CommonHeader,
-      CommonFooter
+import CommonHeader from "@/components/CommonHeader.vue";
+import CommonFooter from "@/components/CommonFooter.vue";
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          name: "电影",
+          path: "/movie",
+          bg: "#00a862"
+        },
+        {
+          name: "音乐",
+          path: "/music",
+          bg: "#456789"
+        },
+        {
+          name: "书籍",
+          path: "/book",
+          bg: "#778898"
+        },
+        {
+          name: "图片",
+          path: "/photo",
+          bg: "pink"
+        }
+      ],
+      selectMenu: {
+        name: "电影",
+        path: "/moive",
+        bg: "#00a862"
+      }
+    };
+  },
+  components: {
+    CommonHeader,
+    CommonFooter
+  },
+  methods: {
+    fn(index){
+      this.selectMenu = this.menu[index];
     }
+  },
+  created(){
+    this.menu.forEach((obj,index)=>{
+      if(obj.path==this.$route.path){
+        this.selectMenu = obj;
+      }
+    })
   }
+};
 </script>
 
 <style>
-
+#content {
+  margin: 1rem 0;
+}
 </style>
