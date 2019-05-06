cfedc7b1fae7e3a7953eaf820d1cb8be9ec6aec2
刘园215 <1803656569@qq.com>
2019-05-06 21:02:47 +0800

    vue

--- new1/src/App.vue
+++ new1/src/App.vue
@@ -1,9 +1,34 @@
 <template>
   <div id="app">
-    
+    <div id="left-nav">
+
+    </div>
+    <div id="right">
+
+    </div>
+    <common-footer></common-footer>
     <router-view/>
   </div>
 </template>
 
+<script>
+import CommonFooter from "@/components/CommonFooter.vue";
+export default {
+  
+}
+</script>
+
 <style>
+#app{
+  display: flex;
+}
+  #left-nav{
+    width: 200px;
+    height:3rem;
+    background: #eee;
+    flex:200px;
+  }
+  #right{
+    flex:1;
+  }
 </style>
---
+++ new1/src/components/CommonFooter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='footer'>
+1111
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .footer{
+        width: 100px;
+        height: 100px;
+        border-top: 1px solid #ccc;
+    }
+</style>
