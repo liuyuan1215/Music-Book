7e411ca453fc3bd30dc7e53d6c840b6b29ba3fcd
刘园215 <1803656569@qq.com>
2019-05-07 19:40:44 +0800

    vue

--- new1/src/components/CommonFooter.vue
+++ new1/src/components/CommonFooter.vue
@@ -1,6 +1,5 @@
 <template>
     <div class='footer'>
-1111
     </div>
 </template>
 
@@ -11,9 +10,4 @@
 </script>
 
 <style scoped>
-    .footer{
-        width: 100px;
-        height: 100px;
-        border-top: 1px solid #ccc;
-    }
 </style>
