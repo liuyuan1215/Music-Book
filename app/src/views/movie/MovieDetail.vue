6f5144f60f40c24595953996b32eaff0137ae41d
刘园215 <1803656569@qq.com>
2019-05-08 15:38:00 +0800

    vue

--- app/src/views/movie/MovieDetail.vue
+++ app/src/views/movie/MovieDetail.vue
@@ -9,7 +9,8 @@
     import Axios from 'axios'
     export default {
         created(){
-            Axios.get(`https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/subject/${this.$route.params.movieId}`)
+            // Axios.get(`https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/subject/${this.$route.params.movieId}`)
+            Axios.get(`/data/movie0.json/${this.$route.params.movieId}`)
             .then((result)=>{
                 this.movieDetail = result.data;
             })
