530d1a598e57900f53f784f150787a1b211a9ef6
刘园215 <1803656569@qq.com>
2019-05-14 18:45:51 +0800

    vue

--- new1/src/views/Three.vue
+++ new1/src/views/Three.vue
@@ -1,15 +1,75 @@
 <template>
     <div>
-11
+        <div id="user">
+            <div id='head-image'>
+                <img src="/user/head-image.png" alt="">
+            </div>
+            <div id='information'>
+                <div>
+                    <h4 id='name'>name</h4>
+                    <p id='introduction'>简介：我在未来等你</p>
+                </div>
+            </div>
+            <div id="more">
+                <button>更多</button>
+            </div>
+        </div>
+
+        <div id='photo'>
+            <img v-for='(obj,index) in $store.state.photoData' :key='index' @click='goDetail(index)' :src="obj.src" alt="">
+        </div>
     </div>
 </template>
 
 <script>
+    import Axios from 'axios'
     export default {
-        
+        created(){
+            Axios.get('/data/photodata.json')
+            .then((result)=>{
+                this.$store.commit('addPhoto',result.data.photoData);
+            })
+        },
+        methods:{
+            goDetail(index){
+                this.$router.push('/photodetail/'+index);
+            }
+        }
     }
 </script>
 
 <style>
-
+    #user{
+        background: #eeeeee;
+        border:1px solid #ccc;
+        height: 1.5rem;
+        display: flex;
+    }
+    #head-image{
+        flex:2;
+    }
+    #head-image img{
+        width: 1rem;
+        height: 1rem;
+        border-radius: 0.5rem;
+        margin: 0.25rem;
+    }
+    #information{
+        flex:4;
+        align-items:center; 
+        display: -webkit-flex;
+    }
+    #more{
+        flex:1;
+        align-items:center; 
+        display: -webkit-flex;
+    }
+    #photo{
+        background: linear-gradient(to bottom,#204060, #d6d9e2);
+        /* background: rgba(35,69,103,0.7); */
+    }
+    #photo>img{
+        width: 44%;
+        margin: 3%;
+    }
 </style>
