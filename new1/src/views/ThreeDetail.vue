3e6cb0cefe29f04088e25461957e120c9829dfd3
刘园215 <1803656569@qq.com>
2019-05-14 18:45:59 +0800

    vue

---
+++ new1/src/views/ThreeDetail.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <router-link to="/three">
+             <v-touch v-on:swipeleft='next()' class='bg' :style="{backgroundImage:'url('+url+')'}"></v-touch>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import VueTouch from 'vue-touch';
+    Vue.use(VueTouch,{name:'v-touch'});
+    export default {
+        data () {
+            return {
+                iNow:this.$route.params.index
+            }
+        },
+        computed:{
+            url(){
+                return this.$store.state.photoData[this.iNow].src
+            }
+        },
+        methods:{
+            next(){
+                this.iNow++;
+                if(this.iNow==this.$store.state.photoData.length){
+                    this.iNow=0;
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .bg{
+        position: absolute;
+        top: 1rem;
+        left: 0;
+        right: 0;
+        bottom: 1rem;
+        background: #000 no-repeat center;
+        background-size: contain;
+    }
+</style>
