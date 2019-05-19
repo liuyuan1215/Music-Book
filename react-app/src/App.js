94275f5fe80b120898b3da9b71bb69bba0b5137c
刘园215 <1803656569@qq.com>
2019-05-19 11:43:56 +0800

    router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
+import Home from './components/Home'
+import A from './components/A'
+import B from './components/B'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to='/'>首页</Link>
+        <Link to='/a'>a页面</Link>
+        <Link to='/b'>b页面</Link>
+        {/* <Route exact path='/' component={Home}></Route>
+        <Route path='/a' component={A}></Route>
+        <Route path='/b' component={B}></Route> */}
+        <Switch>
+          <Route path='/a' component={A}></Route>
+          <Route path='/b' component={B}></Route>
+          <Route path='/' component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ react-app/src/components/A.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class A extends Component {
+  render() {
+    return (
+      <div>
+        11111
+      </div>
+    )
+  }
+}
+
