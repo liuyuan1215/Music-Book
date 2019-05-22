838173ea30371efe21ff259f0ceb7b2644e139d7
刘园215 <1803656569@qq.com>
2019-05-22 16:21:37 +0800

    redux

---
+++ react-app/src/action.js
@@ -0,0 +1,15 @@
+export let add = (count) =>{
+    console.log('触发了Add action');
+    return {
+        type:'ADD',
+        payload:count
+    }
+}
+
+export let reduce = () =>{
+    console.log('reduce');
+    return {
+        type:'REDUCE'
+    }
+}
+// export default add;
