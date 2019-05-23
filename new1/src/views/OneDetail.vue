7d5bd6f6cc0d6d7308305ed546226a977eda9c16
刘园215 <1803656569@qq.com>
2019-05-23 22:14:03 +0800

    vue

---
+++ new1/src/views/OneDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    1
+    <br>1
+    <br>1
+    <br>
+    1
+    {{book.title}}
+    <img v-if="isShow" :src="book.images.small">
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  data() {
+    return {
+      movie: {},
+      isShow: false
+    };
+  },
+  created() {
+    Axios.get("/data/book.json"+this.$route.params.id)
+      .then((result) => {
+        this.book = result.data;
+        this.isShow = true;
+      })
+      .catch(() => {});
+  }
+};
+</script>
+
+<style>
+</style>
