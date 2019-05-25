4fa122d7dd8622a971bfe85cce72f32a031408fc
刘园215 <1803656569@qq.com>
2019-05-25 22:40:28 +0800

    react

---
+++ antd-demo/src/component/CFooter/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Row } from 'antd'
+import './index.css'
+
+export default class CFooter extends Component {
+    render() {
+        return (
+            <div>
+                <Row className='footer'>
+                    html&nbsp;css&nbsp;javascript&nbsp;sass&nbsp;vue&nbsp;react&nbsp;node
+                </Row>                
+            </div>
+        )
+    }
+}
