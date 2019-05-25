15738d97d3f910fda94d58b4afd4c20b2e59f489
刘园215 <1803656569@qq.com>
2019-05-25 15:36:29 +0800

    react

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,39 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Button, Icon, Calendar } from 'antd';
 import './App.css';
-
+import { Row , Col } from 'antd';
+import NavLeft from './component/NavLeft'
 
 function App() {
-  function onPanelChange(value, mode) {
-    // console.log(value, mode);
-  }
-
-
   return (
     <div className="App">
-      <Button type="primary">1111</Button>
-      <div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-        <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-      </div>
-      <Icon type="login" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Row>
+          <Col span={4} className='navleft' >
+              <NavLeft></NavLeft>
+          </Col>
+          <Col span={20}>right</Col>
+        </Row>
     </div>
   );
 }
-
 
 export default App;
