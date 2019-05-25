75863362face446a9891c9755959c7e71367cab8
刘园215 <1803656569@qq.com>
2019-05-25 22:39:41 +0800

    react

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import { Row , Col } from 'antd';
 import NavLeft from './component/NavLeft'
+import CHeader from './component/CHeader'
+import CFooter from './component/CFooter'
+import Home from './page/Home/index'
 
 function App() {
   return (
@@ -10,7 +13,11 @@ function App() {
           <Col span={4} className='navleft' >
               <NavLeft></NavLeft>
           </Col>
-          <Col span={20}>right</Col>
+          <Col span={20}>
+            <CHeader></CHeader>
+            <Home></Home>
+            <CFooter></CFooter>
+          </Col>
         </Row>
     </div>
   );
