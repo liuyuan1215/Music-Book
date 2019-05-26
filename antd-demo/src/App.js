bdee0aff6130907cafcd26667dace19d90ad8456
刘园215 <1803656569@qq.com>
2019-05-26 15:01:28 +0800

    react

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
-import { Row , Col } from 'antd';
+import { Row, Col } from 'antd';
 import NavLeft from './component/NavLeft';
 import CHeader from './component/CHeader';
 import CFooter from './component/CFooter';
-import Home from './page/Home/index';
+//import Home from './page/Home/index';
 
 function App(props) {
   return (
     <div className="App">
-        <Row>
-          <Col span={4} className='navleft' >
-            <NavLeft></NavLeft>   
-          </Col>
-          <Col span={20}>
-            <CHeader></CHeader>
-            {/* <Home></Home> */}
+      <Row>
+        <Col span={4} className='navleft' >
+          <NavLeft></NavLeft>
+        </Col>
+        <Col span={20}>
+          <CHeader></CHeader>
+          {/* <Home></Home> */}
+          <div>
             {props.children}
-            <CFooter></CFooter>
-          </Col>
-        </Row>
+          </div>
+          <CFooter></CFooter>
+        </Col>
+      </Row>
     </div>
   );
 }
