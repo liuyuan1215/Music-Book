74ee2849e61e8a96af6022fbe36c65d0d40fcb98
刘园215 <1803656569@qq.com>
2019-05-26 15:58:07 +0800

    react

--- antd-demo/src/page/StudentList/index.js
+++ antd-demo/src/page/StudentList/index.js
@@ -1,38 +1,55 @@
 import React, { Component } from 'react'
 import { Table } from 'antd';
+import axios from 'axios'
 
 export default class index extends Component {
+  constructor(params){
+    super(params);
+    this.state = {
+      dataSource:[]
+    }
+  }
+  componentWillMount(){
+    axios.get('https://easy-mock.com/mock/5cea36ce8347da71af1d4b52/test/test')
+    .then((res)=>{
+      console.log(res.data.data.projects);
+      this.setState({
+        dataSource:res.data.data.projects
+      })
+    })
+  }
   render() {
     const columns = [
       {
-        title: 'Name',
+        title: '姓名',
         dataIndex: 'name',
-        width: 150,
+        key: 'name',
       },
       {
-        title: 'Age',
-        dataIndex: 'age',
-        width: 150,
+        title: '邮箱',
+        dataIndex: 'email',
+        key: 'email',
       },
       {
-        title: 'Address',
+        title: '地址',
         dataIndex: 'address',
+        key: 'address',
       },
+      {
+        title: '学号',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '学校',
+        dataIndex: 'school',
+        key: 'school',
+      }
     ];
-
-    const data = [];
-    for (let i = 1; i < 101; i++) {
-      data.push({
-        key: i,
-        name: `Edward King ${i}`,
-        age: 32,
-        address: `London, Park Lane no. ${i}`,
-      });
-    }
     return (
       <div>
         <div className='middle'>
-          <Table className='list-table' columns={columns} dataSource={data} pagination={{ pageSize: 10 }} scroll={{ y: 400 }} />
+          <Table className='list-table' columns={columns} dataSource={this.state.dataSource} pagination={{ pageSize: 10 }} scroll={{ y: 400 }} />
         </div>
       </div>
     )
