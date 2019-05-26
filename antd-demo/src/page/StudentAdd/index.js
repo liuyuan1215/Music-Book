70544fb9251feb486be98783cd9c8eeac95acd80
刘园215 <1803656569@qq.com>
2019-05-26 12:45:06 +0800

    react

---
+++ antd-demo/src/page/StudentAdd/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './index.css'
+import { Row } from 'antd'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <Row className='middle'>
+                    <div className='center'>
+                        添加
+                    </div>                   
+                </Row>
+            </div>
+        )
+    }
+}
