243f71e83ff2358288276a8cfc4c3249ebbac372
刘园215 <1803656569@qq.com>
2019-05-30 19:54:38 +0800

    react

--- antd-demo/src/page/StudentList2/index.js
+++ antd-demo/src/page/StudentList2/index.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import { Table } from 'antd';
+import axios from 'axios'
 
 export default class index extends Component {
-    render() {
-        return (
-            <div>
-                33
-            </div>
-        )
+  constructor(params){
+    super(params);
+    this.state = {
+      dataSource:[]
     }
+  }
+  componentWillMount(){
+    axios.get('https://easy-mock.com/mock/5cea36ce8347da71af1d4b52/test/score')
+    .then((res)=>{
+      console.log(res.data.data.projects);
+      this.setState({
+        dataSource:res.data.data.projects
+      })
+    })
+  }
+  render() {
+    const columns = [
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '学号',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '成绩',
+        dataIndex: 'number',
+        key: 'number',
+      }
+    ];
+    return (
+      <div>
+        <div className='middle'>
+          <Table className='list-table' columns={columns} dataSource={this.state.dataSource} pagination={{ pageSize: 10 }} scroll={{ y: 400 }} />
+        </div>
+      </div>
+    )
+  }
 }
