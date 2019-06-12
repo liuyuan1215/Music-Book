f2a7a90b440453c308db583b0a9801d492a07aed
刘园215 <1803656569@qq.com>
2019-06-12 15:01:12 +0800

    node

--- node/express1/controllers/user.js
+++ node/express1/controllers/user.js
@@ -1,30 +1,39 @@
-var User_model=require("../models/user_model.js");
-exports.reg=function(req,res,next){
+var User_model = require("../models/user_model.js");
+
+exports.reg = function (req, res, next) {
 	res.render("reg.ejs");
 }
 
-exports.do_reg=function(req,res,next){
-	var name=req.body.uname;
-	var pass=req.body.pass;
+exports.do_reg = function (req, res, next) {
+	var name = req.body.uname;
+	var pass = req.body.pass;
 	//console.log(name);
 	//console.log(pass);
-	User_model.insert_data(name,pass,function(err,data){
-		//console.log(data);
-		if(data.affectedRows>0){
-			res.redirect('/login');
+
+	//验证用户名是否重名
+	User_model.checkName(name, function (err, data) {
+		if (data.length > 0) {
+			res.redirest('/reg');
+		} else {
+			User_model.insert_data(name, pass, function (err, data) {
+				//console.log(data);
+				if (data.affectedRows > 0) {
+					res.redirect('/login');
+				}
+			})
 		}
-	})	
+	})
 }
 
-exports.login=function(req,res,next){
+exports.login = function (req, res, next) {
 	res.render("login.ejs");
 }
 
-exports.do_login=function(req,res,next){
-	var name=req.body.uname;
-	var pass=req.body.pass;
+exports.do_login = function (req, res, next) {
+	var name = req.body.uname;
+	var pass = req.body.pass;
 
-	User_model.sel_name_by_pass(name,pass,function(err,data){
+	User_model.sel_name_by_pass(name, pass, function (err, data) {
 		console.log(data);
 	})
 
