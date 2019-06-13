951186e20afddb8c3df9aa4d5e3a35657ee5749d
刘园215 <1803656569@qq.com>
2019-06-13 15:40:41 +0800

    node

--- node/express1/controllers/user.js
+++ node/express1/controllers/user.js
@@ -1,4 +1,50 @@
+//var async=require('async');
 var User_model = require("../models/user_model.js");
+
+exports.yy = function (req, res, next) {
+	var name = req.params.username;
+	console.log(name);
+}
+
+exports.zz = function (req, res, next) {
+	var qur = req.query.q;
+	console.log(qur);
+}
+
+exports.chexkajax = function (req, res, next) {
+	var name = req.body.value;
+	// res.send('success');
+	User_model.checkName(name, function (err, data) {
+		if (data.length > 0) {
+			res.send('success');
+		} else {
+			res.send('error');
+		}
+	});
+}
+
+exports.unlogin = function (req, res, next) {
+	req.session = null;
+	res.redirect('/');
+}
+
+exports.do_login = function (req, res, next) {
+	var name = req.body.uname;
+	var pass = req.body.pass;
+
+	User_model.sel_name_by_pass(name, pass, function (err, data) {
+		if (data.length > 0) {
+			req.session = data[0];
+			res.redirect('/');
+		} else {
+			res.redirect('/login');
+		}
+	});
+}
+
+exports.login = function (req, res, next) {
+	res.render('login.ejs');
+}
 
 exports.reg = function (req, res, next) {
 	res.render("reg.ejs");
@@ -11,6 +57,53 @@ exports.do_reg = function (req, res, next) {
 	//console.log(pass);
 
 	//验证用户名是否重名
+
+	/*var asyncFunc = function (name) {
+		return new Promise(function (resolve, reject) {
+			User_model.checkName(name, function (err, data) {
+				resolve(data);
+			})
+		})
+	}
+
+	async function asyncFunction(name,pass){
+		var data=await asyncFunc(name);
+		if(data.length>0){
+			res.redirect('/reg');
+		}else{
+			User_model.insert_data(name,pass,function(err,data){
+				if(data.affectedRows>0){
+					res.redirect('/login');
+				}
+			});
+		}
+	};
+	asyncFuncNew(name,pass);*/
+
+	//async waterfall解决回调地域问题
+	/*async.waterfall([
+		function(callback){
+			User_model.checkName(name,function(err,data){
+				callback(null,data);
+			});
+		},
+		function(data,callback){
+			// arg1 now equals 'one' and arg2 now equals 'two'
+			//callback(null, 'three');
+			if(data.length>0){
+				res.redirect('/reg');
+			}else{
+				User_model.insert_data(name,pass,function(err,data){
+					if(data.affectedRows>0){
+						res.redirect('/login');
+					}
+				});
+			}
+		},
+	],function(err,result){
+		console.log(result);
+	});*/
+
 	User_model.checkName(name, function (err, data) {
 		if (data.length > 0) {
 			res.redirest('/reg');
@@ -23,18 +116,4 @@ exports.do_reg = function (req, res, next) {
 			})
 		}
 	})
-}
-
-exports.login = function (req, res, next) {
-	res.render("login.ejs");
-}
-
-exports.do_login = function (req, res, next) {
-	var name = req.body.uname;
-	var pass = req.body.pass;
-
-	User_model.sel_name_by_pass(name, pass, function (err, data) {
-		console.log(data);
-	})
-
 }
