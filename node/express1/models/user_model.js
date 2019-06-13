74c4b11c55d9b433f64301e19a572e137a80d317
刘园215 <1803656569@qq.com>
2019-06-13 15:40:58 +0800

    node

--- node/express1/models/user_model.js
+++ node/express1/models/user_model.js
@@ -1,9 +1,13 @@
 var db=require("./db.js");
 
-
 exports.insert_data=function(name,pass,callback){
 	var sql="insert into user(uname,pass) values(?,?)";
 	db.query(sql,[name,pass],callback);
+}
+
+exports.checkName=function(name,callback){
+	var sql='select * from user where unmae=?';
+	db.query(sql,[name],callback);
 }
 
 exports.sel_name_by_pass=function(name,pass,callback){
