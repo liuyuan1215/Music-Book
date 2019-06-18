04ed9ebd140992aa85009d20f9be112318599ba3
刘园215 <1803656569@qq.com>
2019-06-18 18:46:35 +0800

    node

--- node/express1/models/db.js
+++ node/express1/models/db.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    connnectionLimit: 10,
+    connectionLimit: 10,
     host: 'localhost',
     user: 'root',
     password: 'admin',
     database: 'node1'
-});
+})
 
 exports.query = function (sql, params, callback) {
-    pool.query(sql,params,function(errors,results,fields){
-        if(error) throw error;
-        callback(error,results);
-    });
+    pool.query(sql, params, function (error, results, fields) {
+        if (error) throw error;
+        callback(error, results);
+    })
 }
