f1e075c9409b25e2fe2eb571f2d2f76e89b61144
刘园215 <1803656569@qq.com>
2019-06-19 22:12:25 +0800

    node

--- node/myblog/controllers/blog.js
+++ node/myblog/controllers/blog.js
@@ -19,19 +19,43 @@ exports.index=function(req,res,next){
 }
 
 exports.add=function(req,res,next){
-    res.render('newBlog',{
-        'sess':req.session
+    var uid=req.session.USER_ID;
+    Blog_model.get_catalogs_by_id(uid,function(err,data){
+        res.render('newBlog',{
+            'sess':req.session,
+            'catalogs':data
+        })
     })
 }
+// exports.add=function(req,res,next){
+// 	res.render("newBlog",{
+// 		"sess":req.session
+// 	});
+// }
 
 exports.do_add=function(req,res,next){
     var title=req.body.title;
     var content=req.body.content;
-    var date=moment().format();
+    var str=moment().format();;
+    var arr=str.split("T");
+    console.log(arr);
+    var date=arr[0];
+    // var date2=arr[1];
+    // var date=[...date1,...date2];
+    // console.log(date);
+    console.log(moment().format());
     var uid=req.session.USER_ID;
-    Blog_model.ins_blog_by_data(title,content,date,uid,function(err,data){
+    var cid=req.body.catalog;
+    Blog_model.ins_blog_by_data(title,content,cid,date,uid,function(err,data){
         if(data.affectedRows>0){
             res.redirect('/index');
         }
+    })
+}
+
+exports.Catalog=function(req,res,next){
+    var uid=req.session.USER_ID;
+    Blog_model.checkCatalog(uid,function(err,data){
+        res.render()
     })
 }
