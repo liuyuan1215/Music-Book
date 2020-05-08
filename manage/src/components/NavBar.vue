253d1c7c0f1fd032e005fc6d9f59bb7449af7e32
刘园215 <1803656569@qq.com>
2020-05-08 23:03:49 +0800

    侧边导航

--- manage/src/components/NavBar.vue
+++ manage/src/components/NavBar.vue
@@ -1,17 +1,24 @@
 <template>
-  <van-tabbar v-model="active" active-color="#6375eb" class="leftnav" @change="change">
+  <!-- <van-tabbar v-model="active" active-color="#6375eb" class="leftnav" @change="change">
     <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
     <van-tabbar-item to="/user">用户管理</van-tabbar-item>
     <van-tabbar-item to="/book">商品管理</van-tabbar-item>
     <van-tabbar-item to="/order">订单管理</van-tabbar-item>
-  </van-tabbar>
+  </van-tabbar>-->
+  <van-sidebar v-model="activeKey" class="leftnav">
+    <van-sidebar-item to="/" title="首页" />
+    <van-sidebar-item to="/user" title="用户管理" />
+    <van-sidebar-item to="/book" title="商品管理" />
+    <van-sidebar-item to="/order" title="订单管理" />
+  </van-sidebar>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      active: 0
+      // active: 0
+      activeKey: 0
     };
   },
   created() {
@@ -26,12 +33,28 @@ export default {
 </script>
 
 <style scoped>
-.leftnav{
+.leftnav {
   text-align: center;
   height: 6vh;
   line-height: 6vh;
   font-size: 18px;
   font-weight: 400;
   cursor: pointer;
+}
+.van-sidebar-item--select::before {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 4px;
+  height: 30px;
+  background-color: #6375eb;
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+  content: "";
+}
+.van-sidebar-item {
+  color: #000;
+  font-size: 18px;
+  font-weight: 400;
 }
 </style>
