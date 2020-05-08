296acb9aa61ea17c56b46c699e94789bf72a7d81
刘园215 <1803656569@qq.com>
2020-05-08 23:03:00 +0800

    滚动条

--- manage/src/main.js
+++ manage/src/main.js
@@ -2,21 +2,49 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button, Icon, NavBar, Tabbar, TabbarItem, Sidebar, SidebarItem,  Col, Row, } from 'vant';
+import { Button, Icon, Sidebar, SidebarItem, NavBar, Tabbar, TabbarItem, Swipe, SwipeItem, Lazyload, Tabs, Tab, CellGroup, Field, Toast, Col, Row, List, PullRefresh, GoodsAction, GoodsActionButton, GoodsActionIcon, NoticeBar, Card, SubmitBar, Sku, Search, AddressList, AddressEdit, Area, Form, Popup, RadioGroup, Radio, Cell} from 'vant';
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
+import '@/assets/css/scrollbar.css'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Tabbar);
+Vue.use(Sidebar);
+Vue.use(SidebarItem);
+Vue.use(TabbarItem);
 Vue.use(NavBar);
 Vue.use(Icon);
 Vue.use(Button);
-Vue.use(Tabbar);
-Vue.use(TabbarItem);
-Vue.use(Sidebar);
-Vue.use(SidebarItem);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(Lazyload);
+Vue.use(Tab);
+Vue.use(Tabs);
+Vue.use(CellGroup);
+Vue.use(Field);
+Vue.use(Toast);
 Vue.use(Col);
 Vue.use(Row);
+Vue.use(List);
+Vue.use(PullRefresh);
+Vue.use(GoodsAction);
+Vue.use(GoodsActionButton);
+Vue.use(GoodsActionIcon);
+Vue.use(NoticeBar);
+Vue.use(Card);
+Vue.use(SubmitBar);
+Vue.use(Sku);
+Vue.use(Search);
+Vue.use(AddressList);
+Vue.use(AddressEdit);
+Vue.use(Area);
+Vue.use(Form);
+Vue.use(Popup);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(Cell);
 
 new Vue({
   router,
