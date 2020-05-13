6e1f7663c144225a5a93e1acb3e09a15846b0551
刘园215 <1803656569@qq.com>
2020-05-13 23:18:39 +0800

    管理员首页

--- manage/src/views/Home.vue
+++ manage/src/views/Home.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="container">首页</div>
+  <div
+    class="container"
+  >{{JSON.stringify(userInfo) === '{}' ? '' : userInfo.userName}} 请 开 始 你 的 工 作 ！</div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   data() {
     return {};
   },
   components: {},
   created() {},
-  computed: {},
+  computed: {
+    ...mapState(["userInfo"])
+  },
   methods: {}
 };
 </script>
 
 <style lang="scss">
 .container {
-  background: #aaa;
-  width: 100%;
-  height: 90vh;
   position: fixed;
   left: 13vw;
+  width: 81vw;
+  margin: 5vh 3vw;
+  height: 80vh;
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #6375eb;
+  font-size: 50px;
+  text-align: center;
+  line-height: 80vh;
 }
 </style>
